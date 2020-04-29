83b910915ca2d90756d0be44d8f39ab4174d61ea
salEZY <bobicaleks@yahoo.com>
2020-04-29 22:00:48 +0200

    start css

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,12 +5,21 @@ export const Nav = () => {
     li1.style = 'list-style: none;'
     const loginBtn = document.createElement('button')
     loginBtn.textContent = 'Login'
-    li1.appendChild(loginBtn)
-
+    loginBtn.addEventListener('click', () => {
+        loginBtn.style.display = 'none'
+        registerBtn.style.display = 'block'
+    })
     const li2 = document.createElement('li')
     li2.style = 'list-style: none;'
     const registerBtn = document.createElement('button')
-    registerBtn.textContent = 'Register'
+    registerBtn.textContent = 'Logout'
+    registerBtn.style = 'display: none; color: white; background-color: black;border: 0;outline: 0; padding: 5px;'
+    registerBtn.addEventListener('click', () => {
+        loginBtn.style.display = 'block'
+        registerBtn.style.display = 'none'
+    })
+
+    li1.appendChild(loginBtn)
     li2.appendChild(registerBtn)
     ul.append(li1, li2)
     return ul
