a7d1735bc78f46495c65058e03c33055883007f4
AleksBobic <bobicaleks@yahoo.com>
2020-05-04 15:14:46 +0200

    overall css refactor

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -3,24 +3,33 @@ import { renderTaskList } from '../layouts/Main'
 
 export const TaskForm = () => {
     const div = document.createElement('div')
-    div.style = 'display: flex; flex-direction: row; justify-content: space-evenly;'
+    div.id = 'task-form'
     const label = document.createElement('label')
-    label.textContent = 'Add task'
+    label.textContent = 'Task description'
     label.htmlFor = 'desc'
     const input = document.createElement('input')
     input.type = 'text'
     input.placeholder = 'Task Name'
     input.id = 'desc'
+    const span = document.createElement('span')
+    span.textContent = 'No task description!'
+    span.style = 'display: none; padding-top: 7.5px; color: darkred;font-weight: bolder;font-size: 18px;'
     const submitBtn = document.createElement('button')
     submitBtn.type = 'submit'
     submitBtn.id = 'addTask'
     submitBtn.className = 'btn'
-    submitBtn.textContent = 'ADD'
+    submitBtn.textContent = 'Add'
     submitBtn.addEventListener('click', () => {
+        if (input.value === '') {
+            span.style.display = 'block'
+            setTimeout(() => {
+                span.style.display = 'none'
+            },3000)
+        }
         addTask(input.value)
         renderTaskList()
     })
 
-    div.append(label, input, submitBtn)
+    div.append(label, input, span, submitBtn)
     return div
 }
--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -2,7 +2,7 @@ import { Nav } from '../components/Nav'
 
 export const Header = () => {
     const header = document.createElement('header')
-    header.style = 'display: flex; flex-direction: row;'
+    header.style = 'display: flex; flex-direction: row; justify-content: space-between;'
 
     const title = document.createElement('h2')
     title.textContent = 'TO DO APP'
