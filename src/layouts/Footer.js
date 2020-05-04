3e1b2215e5089da31526ce85d849ea68adc47e7c
salEZY <bobicaleks@yahoo.com>
2020-05-04 22:23:32 +0200

    add link to footer

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,8 +1,14 @@
 export const Footer = () => {
     const footer = document.createElement('footer')
+    footer.style = 'display: flex; flex-direction: row; justify-content: space-between'
     const h4 = document.createElement('h4')
     h4.textContent = `Copyright \u00A9 2020`
+    const git = document.createElement('a')
+    git.textContent = 'GitHub - salEZY'
+    git.href = 'https://github.com/salEZY'
+    git.target = '_blank'
+    git.style = 'margin: 18px 20px; text-decoration: none; color: #a0785f; font-size: 20px;font-weigth: bolder; text-shadow: 2px 2px 1px white'
 
-    footer.appendChild(h4)
+    footer.append(h4, git)
     return footer
 }
