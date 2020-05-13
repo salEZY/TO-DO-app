bda3e4683a552e2b1b8dafb61097a3640cea481f
AleksBobic <bobicaleks@yahoo.com>
2020-05-13 22:33:16 +0200

    first draft canvas done

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -3,13 +3,24 @@ div.className = 'intro-section'
 
 export const Intro = () => {
     const canvas = document.createElement('canvas')
-    canvas.width = innerWidth
-    canvas.height = innerHeight
+    canvas.id = 'myCanvas'
     let ctx = canvas.getContext('2d')
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    setInterval(() => {drawTriangle(ctx)}, 1000)
+    setInterval(() => {
+        drawTriangle(canvas, ctx)
+    }, 1000)
+    setInterval(() => {
+        ctx.fillStyle = "black";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+    },20000)  
     div.appendChild(canvas)
+    const h1 = document.createElement('h1')
+    h1.textContent = 'Welcome TO DO App'
+    h1.className = 'title-list'
+    const p = document.createElement('p')
+    p.textContent = 'Login to start adding to do tasks!'
+    div.append(h1, p)
     return div
 }
 
@@ -17,23 +28,18 @@ export const hideIntro = (bool) => {
     bool ? div.style.display = 'none' : div.style.display = 'block'
 }
 
-const drawTriangle = (ctx) => {
-    let a = generateRandom(2000)
-    let b = generateRandom(700)
-    let c = generateRandom(2000)
+const drawTriangle = (canvas, ctx) => {
+    let a = generateRandom(canvas.width)
+    let b = generateRandom(canvas.height)
+    let c = generateRandom(canvas.height)
     ctx.beginPath();
     ctx.strokeStyle = 'cadetblue'
-    ctx.lineWidth = generateRandom(5)
+    ctx.lineWidth = generateRandom(1)+1.5
     ctx.moveTo(a, b);
-    ctx.lineTo(b, c);
     ctx.lineTo(a, c);
-    if (Math.random() > .5) {
-        ctx.fillStyle = 'cadetblue'
-        ctx.fill()
-    }else{
-        ctx.closePath();
-        ctx.stroke();
-    } 
+    ctx.lineTo(b, c);
+    ctx.closePath();
+    ctx.stroke();
 }
 
 const generateRandom = (num) => {
